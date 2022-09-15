3d1384f941b406b65b5ebf3ec0b75a85c9951e45
AnirudhMoolya8 <anirudhmoolya1999@gmail.com>
2022-09-15 18:07:14 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')  //not adding (session) at the end here
 const connectDB = require('./config/db')
+const { default: mongoose } = require('mongoose')
 
 //Load config
 dotenv.config({ path: './config/config.env' })
@@ -20,6 +23,23 @@ const app = express()
 
 
 
+//Body parser --> basically making the form to add stories work
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+
+
+//Method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
+
 
 
 
@@ -29,7 +49,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 
-
+//Handlebars helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
 
 
 
@@ -37,6 +58,13 @@ if (process.env.NODE_ENV === 'development') {
 //Handlebars code to shorten to .hbs
 // Added word .engine since its been updated
 app.engine('.hbs', exphbs.engine({
+    helpers: {
+        formatDate,
+        stripTags,
+        truncate,
+        editIcon,
+        select
+    },
     defaultLayout: 'main',
     extname: '.hbs'
 })
@@ -49,7 +77,10 @@ app.set('view engine', '.hbs');
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI
+    })
 }))
 
 
@@ -60,6 +91,11 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 
+//Set global var
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null           //Uses a global variable to access user value thrpugh passport just above
+    next()                                       //Literally just triggers the next piece of middleware so the sequence of functions doesn't break
+})
 
 
 
@@ -75,6 +111,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 //Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 
 
