f5c00dbff8784074f84232d77c4698d8a8308f93
AnirudhMoolya8 <anirudhmoolya1999@gmail.com>
2022-09-16 09:49:16 -0400

    Modified files

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')            //bring in multiple items and do the same process
 
+const Story = require('../models/Story')
+
 //@desc login/landing page  --> BASICALLY route for login page
 //@route GET /
 router.get('/', ensureGuest, (req, res) => {
@@ -12,9 +14,21 @@ router.get('/', ensureGuest, (req, res) => {
 
 //@desc dashboard page  --> BASICALLY route for dashboard page
 //@route GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-    console.log(req.user)
-    res.render('dashboard')
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()                //Take everything in story and find user data which will then render into handlebar page.
+        console.log(req.user)                                                         //lean() method converts data to JSON so handlebars can understand
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+
+
+
 })
 
 
